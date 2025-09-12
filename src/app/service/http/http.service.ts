9a94bdecd1592a626a7ac5e12a6bdc864000f004
poojangit <poojang147@gmail.com>
2025-09-13 01:01:48 +0530

    [POOJA] Added : implement login & signup components with guard and services
    - implemented login-signup component with form validation and UI integration
    - created auth guard to restrict access to protected routes
    - built reusable http service for API communication

---
+++ src/app/service/http/http.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private readonly http: HttpClient) {}
+
+  token = '';
+
+  getHeader(): void {
+    this.token = localStorage.getItem('token') ?? '';
+  }
+
+  getAPI(url: string, options: any = {}) {
+    return this.http.get(url, options);
+  }
+
+  postAPI(url: string, body: any, options: any = {}) {
+    return this.http.post(url, body, options);
+  }
+
+  deleteAPI(url: string, options: any = {}) {
+    return this.http.delete(url, options);
+  }
+
+  putAPI(url: string, body: any, options: any = {}) {
+    return this.http.put(url, body, options);
+  }
+}
